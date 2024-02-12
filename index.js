3fc207e1601ec4dc5c233475c758cd9a46083e35
Sandro Möbius <Sandro.moebius@gob.de>
2024-02-12 18:56:24 +0100

    NotWorkingAtAllButSomeCode

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-import { writeFile } from "node:fs";
-import { Buffer } from 'node:buffer';
+import fs from "fs";
 import inquirer from "inquirer";
 import qr from "qr-image";
 
+const prompt = inquirer.createPromptModule();
+
+inquirer
+    .prompt([{
+        "message": "Type in your URL:",
+        name: "URL"
+    }])
+    .then((answers) => {
+        const url = answers.URL;
+        var urlImage = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream("qr-image.png"));
+
+        fs.writeFile("UserUrl.txt", url, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        }); 
+    })
+    .catch((error) => {
+    if (error.isTtyError) {
+        //"Prompt couldn't be rendered in the current environment";
+    } else {
+        //"Something else went wrong";
+    }
+    });
